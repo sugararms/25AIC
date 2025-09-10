b5cc241830c0945339e207a25987eb890b5c3c80
sugararms <gunwoo0908@kakao.com>
2025-09-10 14:24:08 +0900

    d

--- Project3/Project3/FileName.c
+++ Project3/Project3/FileName.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
 int main(void) {
+    int width = 10;   
+    int height = 6;   
+    int row, col;
 
-    printf(
-        "\x1B[3;3H**********"
-        "\x1B[4;3H*        *"
-        "\x1B[5;3H*        *"
-        "\x1B[6;3H*        *"
-        "\x1B[7;3H*        *"
-        "\x1B[8;3H**********"
-    );
+    for (row = 0; row < height; row++) {
+        for (col = 0; col < width; col++) {
+            
+            if (row == 0 || row == height - 1 || col == 0 || col == width - 1) {
+              
+                printf("\x1B[%d;%dH#", row + 3, col + 3);
+             
+            }
+        }
+    }
 
     return 0;
 }
